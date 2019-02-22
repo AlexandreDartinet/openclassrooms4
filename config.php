53f84a6220582e51cd152653a5efca2794f09947
AlexandreDartinet <alexandre.dartinet@gmail.com>
2019-02-22 13:26:12 +0100

    Choice in config to use PHPMailer or default php mail() function

--- config.php
+++ config.php
@@ -4,9 +4,10 @@ define("DB_USER","XXX");
 define("DB_PASSWORD","XXX");
 define("DB_BASE","XXX");
 
+define("USE_PHPMAILER", false);
 define("SMTP_HOST","XXX");
 define("SMTP_USER","XXX");
 define("SMTP_PASSWORD","XXX");
-define("SMTP_PORT",0);
+define("SMTP_PORT",465);
 define("CONTACT_MAIL","XXX");
 define("PHPMAILER_PATH", "model/PHPMailer/");
