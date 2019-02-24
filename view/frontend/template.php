6f6e8f98573e8a6e6803b2004d94f0732b7d130e
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-02-24 12:35:38 +0100

    Added jquery

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -15,6 +15,7 @@ require('footer.php');
         <link href="/public/css/style.css" rel="stylesheet" /> 
         <meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="mobile-web-app-capable" content="yes">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
         <title><?= $title ?></title>
     </head>
         
