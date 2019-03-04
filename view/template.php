6ef86d1ae28c0d1424907bc10ced5802a3939390
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-03-04 11:20:37 +0100

    Added favicon

--- view/template.php
+++ view/template.php
@@ -10,7 +10,8 @@ require('footer.php');
     <head>
         <meta charset="utf-8" />
         <meta name="robots" content="noindex,nofollow"/>
-		<meta name="author" content="Alexandre Dartinet"/>
+        <meta name="author" content="Alexandre Dartinet"/>
+        <link rel="Shortcut Icon" href="/public/images/favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
         <link href="/public/css/style.css" rel="stylesheet" /> 
         <meta name="viewport" content="width=device-width, initial-scale=1">
